d10a258a14761e4a5542c1ff45f548cc4b8e0686
Philip Tibom <philiptibom@gmail.com>
2015-04-01 14:35:07 +0200

    libGDX project created

---
+++ project/desktop/src/se/computerscience/kelde/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.computerscience.kelde.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.computerscience.kelde.KeldeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KeldeGame(), config);
+	}
+}
