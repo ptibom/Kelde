65308fa79774faf1cad7959cb9b76928201b9c15
Philip Tibom <philiptibom@gmail.com>
2015-04-07 19:55:50 +0200

    Added working collision with walls. Need to re-design the code

--- project/core/src/se/computerscience/kelde/view/MapScreen.java
+++ project/core/src/se/computerscience/kelde/view/MapScreen.java
@@ -5,17 +5,19 @@
 package se.computerscience.kelde.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import net.dermetfan.gdx.physics.box2d.Box2DMapObjectParser;
 
 
@@ -25,6 +27,9 @@ public class MapScreen implements Screen {
     private TiledMap map; // Loads the map
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
+    private Body body;
+    private Texture texture;
+    private SpriteBatch batch;
 
     @Override
     public void show() {
@@ -36,16 +41,53 @@ public class MapScreen implements Screen {
         box2DDebugRenderer = new Box2DDebugRenderer();
         Box2DMapObjectParser parser = new Box2DMapObjectParser();
         parser.load(world, map.getLayers().get("Collision"));
+
+
+        // Create a player
+        BodyDef def = new BodyDef();
+        def.position.set(70, 50);
+        def.type = BodyType.DynamicBody;
+        body = world.createBody(def);
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(16, 24);
+        fdef.shape = shape;
+        body.createFixture(fdef);
+
+        texture = new Texture("testSprite.png");
+        batch = new SpriteBatch();
     }
     @Override
     public void render(float delta) {
         // Renders the scene, first clear it with black.
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        int x = 0, y = 0;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            y = 100;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            y = -100;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            x = 100;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            x = -100;
+        }
+        body.setLinearVelocity(x, y);
+        world.step(delta, 6, 2);
         // Choose to render from Camera's perspective and then render it.
         renderer.setView(camera);
         renderer.render();
+
+        Sprite player = new Sprite(texture, 32, 48);
+        player.setPosition(body.getPosition().x - 16, body.getPosition().y - 24);
+
+        batch.begin();
+        player.draw(batch);
+        batch.end();
+
         box2DDebugRenderer.render(world, camera.combined);
     }
 
@@ -62,6 +104,8 @@ public class MapScreen implements Screen {
         camera.viewportHeight = height;
         camera.position.set(width / 2, height / 2, 0); // Temporary camera position. Divide by 2 to make the map stick by the corner.
         camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
     }
 
     @Override
