ab4e7aae4416bc48af23fe8261eab54ba9359da8
Philip Tibom <philiptibom@gmail.com>
2015-04-07 17:29:46 +0200

    Created collision map

--- project/core/src/se/computerscience/kelde/view/MapScreen.java
+++ project/core/src/se/computerscience/kelde/view/MapScreen.java
@@ -8,14 +8,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import net.dermetfan.gdx.physics.box2d.Box2DMapObjectParser;
+
 
 public class MapScreen implements Screen {
     private OrthographicCamera camera; // Camera is the view from where the scene is rendered.
     private OrthogonalTiledMapRenderer renderer; // Renders the map
     private TiledMap map; // Loads the map
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
 
     @Override
     public void show() {
@@ -23,17 +32,21 @@ public class MapScreen implements Screen {
         map = new TmxMapLoader().load("map.tmx");
         camera = new OrthographicCamera();
         renderer = new OrthogonalTiledMapRenderer(map);
+        world = new World(new Vector2(0, 0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+        Box2DMapObjectParser parser = new Box2DMapObjectParser();
+        parser.load(world, map.getLayers().get("Collision"));
     }
-
     @Override
     public void render(float delta) {
         // Renders the scene, first clear it with black.
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Choose to render from Camera's perspective and then render it.
         renderer.setView(camera);
         renderer.render();
+        box2DDebugRenderer.render(world, camera.combined);
     }
 
     @Override
