2015f4df958f8fd2986ca9b3fac7213da92fbc4a
Anders Bolin <anders@bolin.nu>
2015-05-20 13:08:33 +0200

    added tests

---
+++ project/core/src/se/computerscience/kelde/model/encapsulation/IMonster.java
@@ -0,0 +1,9 @@
+package se.computerscience.kelde.model.encapsulation;
+
+/**
+ * Created by Anders on 2015-05-19.
+ */
+public interface IMonster {
+
+
+}
---
+++ project/core/test/se/computerscience/kelde/model/entities/MonsterTest.java
@@ -0,0 +1,7 @@
+package se.computerscience.kelde.model.entities;
+
+/**
+ * Created by Anders on 2015-05-19.
+ */
+public class MonsterTest {
+}
