dd676c5f20f2e9eeb730d473f8be5dc5974249cb
Hossein Hussain <angryregen@gmail.com>
2015-05-21 13:58:34 +0200

    lavascreen added, for screenchanging

---
+++ project/core/src/se/computerscience/kelde/screens/LavaScreen.java
@@ -0,0 +1,57 @@
+/**
+ * Description:
+ *
+ * @author: Hossein Hussain
+ */
+package se.computerscience.kelde.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import se.computerscience.kelde.controller.gameworld.LavaWorldController;
+
+public class LavaScreen implements Screen {
+    private LavaWorldController lavaWorldController;
+    @Override
+    public void show() {
+        // Initialises objects, like a constructor
+        lavaWorldController = new LavaWorldController();
+        //Gdx.input.setInputProcessor(new InputController(lavaWorldController));
+    }
+
+    @Override
+    public void render(float delta) {
+        // Renders the scene, first clear it with black.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Render the world based om woth screen is set.
+        lavaWorldController.render(delta);
+
+    }
+    @Override
+    public void resize(int width, int height) {
+        lavaWorldController.resizeCamera(width, height);
+    }
+
+    @Override
+    public void pause() {
+        //TODO: Pause
+    }
+
+    @Override
+    public void resume() {
+        //TODO: Resume
+    }
+
+    @Override
+    public void hide() {
+        // When screen is no longer used, dispose the objects.
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        lavaWorldController.dispose();
+    }
+}
