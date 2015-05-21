aa78953f5c08e98d1afa33ddb71389b276face1f
Anders Bolin <anders@bolin.nu>
2015-05-21 14:01:26 +0200

    added Iworld

---
+++ project/core/src/se/computerscience/kelde/controller/gameworld/IGameWorldController.java
@@ -0,0 +1,11 @@
+package se.computerscience.kelde.controller.gameworld;
+
+/**
+ * Created by Anders on 2015-05-21.
+ */
+public interface IGameWorldController {
+
+    public void setKeyDown(int keycode);
+
+    public void setKeyUp(int keycode);
+}
