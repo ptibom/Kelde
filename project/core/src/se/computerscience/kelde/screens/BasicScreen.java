e4d5d5e5650cf51c88e10328b19cef6230f48f73
Hossein Hussain <angryregen@gmail.com>
2015-05-21 18:40:19 +0200

    added files that was missing in the last commit, basicScreen

---
+++ project/core/src/se/computerscience/kelde/screens/BasicScreen.java
@@ -0,0 +1,11 @@
+/**
+ * Description: Make methods visible in Screens.
+ *
+ * @author: Hossein Hussain
+ */
+package se.computerscience.kelde.screens;
+
+public abstract class BasicScreen
+{
+    public abstract void cleanEventBuses();
+}
