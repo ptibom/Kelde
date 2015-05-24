da7b6f6c99f5fbafec05591d00b68f78d8bb0ec3
Anders Bolin <anders@bolin.nu>
2015-05-24 14:57:14 +0200

    fixed pmd issues

--- project/core/src/se/computerscience/kelde/view/worldobjects/CampFireView.java
+++ project/core/src/se/computerscience/kelde/view/worldobjects/CampFireView.java
@@ -12,20 +12,19 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import se.computerscience.kelde.model.worldobjects.CampFire;
 
 public class CampFireView implements IWorldObjectView {
-    private final static String SPRITE_LOCATION = "CampFire.atlas";
+    private static final String SPRITE_LOCATION = "CampFire.atlas";
     private final CampFire campFire;
-    private TextureAtlas textureAtlas;
-    private Animation animation;
-    private float elapsedTime = 0, delta = 0;
+    private float elapsedTime, delta;
 
 
     public CampFireView(CampFire campFire) {
         this.campFire = campFire;
-        textureAtlas = new TextureAtlas(Gdx.files.internal(SPRITE_LOCATION));
-        animation = new Animation(0.15f, textureAtlas.getRegions());
+
     }
     @Override
     public void draw (SpriteBatch batch) {
+        final TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal(SPRITE_LOCATION));
+        final Animation animation = new Animation(0.15f, textureAtlas.getRegions());
         elapsedTime += delta;
         if (elapsedTime >= 10f){
             elapsedTime = 0;
