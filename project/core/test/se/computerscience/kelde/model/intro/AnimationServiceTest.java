1482e2840b1843b71538286b4c1e1b10ae22e10c
Daniel Olsson <daniel.dsc.olsson@gmail.com>
2015-05-31 10:49:50 +0200

    looking at test path

--- project/core/test/se/computerscience/kelde/model/intro/AnimationServiceTest.java
+++ project/core/test/se/computerscience/kelde/model/intro/AnimationServiceTest.java
@@ -24,21 +24,32 @@ public class AnimationServiceTest {
     private final Charset charset = Charset.forName("UTF-8");
     private final List<List<String>> allData = new ArrayList<List<String>>();
     private Intro introModel;
-    
 
     @Before
     public void initTest() {
         try {
+            
+            File file = new File("/core/assets/intro/introsprites.txt");
+            File file2 = new File("/core/assets/intro/introtalk.txt");
+            File file3 = new File("/core/assets/intro/animationdemon.intro");
+            File file4 = new File("/core/assets/intro/animationwizardinstr.intro");
+            File file5 = new File("/core/assets/intro/animationwizardinstr.intro");
+            File file6 = new File("/core/assets/intro/animationwizard2instr.intro");
+            File file7 = new File("/core/assets/intro/animationwizarddialogue.intro");
+            File file8 = new File("/core/assets/intro/animationdemondialog.intro");
+            File file9 = new File("/core/assets/intro/animationspellinstr.intro");
 
-            allData.add(Files.readAllLines(Paths.get(("./intro/introsprites.txt")), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/introtalk.txt"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/spell.txt"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationdemon.intro"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationwizardinstr.intro"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationwizard2instr.intro"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationwizarddialogue.intro"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationdemondialog.intro"), charset));
-            allData.add(Files.readAllLines(Paths.get("C:/FixingTheBranch/sixthTry/EIGTHTRY/Kelde/project/core/assets/intro/animationspellinstr.intro"), charset));
+            System.out.println(file.getAbsoluteFile());
+            allData.add(Files.readAllLines(Paths.get((file.getAbsolutePath())), charset));
+
+            allData.add(Files.readAllLines(Paths.get(file2.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file3.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file4.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file5.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file6.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file7.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file8.getAbsolutePath()), charset));
+            allData.add(Files.readAllLines(Paths.get(file9.getAbsolutePath()), charset));
 
             introModel = new Intro(allData);
         } catch (IOException e) {
